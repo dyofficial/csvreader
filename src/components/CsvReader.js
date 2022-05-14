08c2549422925ce34bf38fe0fd42306f319e5709
Daniel Yeboah <danielkofi4@gmail.com>
2022-05-14 15:17:02 +0000

    improved csv reader logic

--- src/components/CsvReader.js
+++ src/components/CsvReader.js
@@ -1,27 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiTrash2 } from "react-icons/fi";
 import { BsPencilSquare } from "react-icons/bs";
-import "../style.css";
+
+const compareFn = (a, b) => {
+  //   if (a > b) return 1;
+  //   if (b > a) return -1;
+  //   return 0;
+};
 
 const CsvReader = () => {
   const [csvFile, setCsvFile] = useState();
   const [csvArray, setCsvArray] = useState([]);
   const [edit, setEdit] = useState(false);
+  const [csvStr, setCsvStr] = useState("");
+  const [headers, setHeaders] = useState([]);
+  const [fileToExport, setFileToExport] = useState();
+
   const [editField, setEditField] = useState({
     firstName: "",
     lastName: "",
     age: "",
     sex: "",
   });
-  const [errorMsg, setErrorMsg] = useState("");
 
   const processCSV = (str, delim = ",") => {
-    var headers = str.slice(0, str.indexOf("\n")).split(delim);
+    console.log("str:::", str);
+    const headers = str
+      .slice(0, str.indexOf("\n"))
+      .split(delim)
+      .sort(compareFn);
+    setHeaders(headers);
     const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+    console.log("rows", rows);
 
     const newArray = rows.map((row, id) => {
       const values = row.split(delim);
-      // console.log("values", values);
+      console.log("values", values);
 
       const eachObject = headers.reduce((obj, header, index) => {
         obj[header] = values[index];
@@ -31,52 +45,88 @@ const CsvReader = () => {
       }, {});
       return eachObject;
     });
-    // console.log(headers);
-    // console.log("newArray", newArray);
+
+    console.log("newArray", newArray);
 
     setCsvArray(newArray);
   };
 
+  useEffect(() => {
+    let csvStr_ = headers.sort(compareFn).join(",");
+
+    csvStr_ = csvArray.reduce((acc, cur) => {
+      const rowStr = Object.values(cur).sort(compareFn).join(",");
+      acc += `\n${rowStr}`;
+      return acc;
+    }, csvStr_);
+
+    setCsvStr(csvStr_);
+  }, [csvArray, headers]);
+
+  console.log("headers", headers);
+  console.log("csvStr", csvStr);
+  console.log("csvArray", csvArray);
+
   const submit = () => {
     const file = csvFile;
-    file === undefined
-      ? setErrorMsg("Please import a file")
-      : setErrorMsg(null);
     const reader = new FileReader();
     reader.onload = function (e) {
       const text = e.target.result;
+      console.log("text", text);
       processCSV(text);
     };
     reader.readAsText(file);
   };
 
   const handleEdit = (id) => {
+    // e.preventDefault();
     setEdit(true);
-    let editableField = csvArray.find((item) => item.id === id);
+    let editableField = csvArray.find((item) => item.id == id);
+    console.log(editableField);
     setEditField(editableField);
+
+    // console.log(e);
   };
   const handleDelete = (id) => {
+    // console.log(firstName);
+    // console.log(csvArray);
+
     const filteredArray = csvArray.filter((item) => item.id !== id);
     setCsvArray([...filteredArray]);
   };
   const handleSubmit = () => {
+    console.log(editField);
+
     const newcsvarray = csvArray.map((item, index) =>
       item.id === editField.id ? editField : item
     );
 
+    console.log(newcsvarray);
+
     setCsvArray(newcsvarray);
-    setEdit(false);
   };
 
   const handleEditField = (e) => {
     setEditField({ ...editField, [e.target.name]: e.target.value });
   };
 
+  function download(file) {
+    const link = document.createElement("a");
+    const url = URL.createObjectURL(file);
+
+    link.href = url;
+    link.download = csvFile.name;
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+  }
+
   return (
     <div className="content">
-      <h3>Crud App</h3>
       {edit ? (
-        <div className="edit">
+        <div>
           <input
             type="text"
             placeholder="First Name"
@@ -92,7 +142,7 @@ const CsvReader = () => {
             name="lastName"
           />
           <input
-            type="text"
+            type="number"
             placeholder="Age"
             onChange={handleEditField}
             value={editField.age}
@@ -112,6 +162,8 @@ const CsvReader = () => {
       ) : null}
 
       <form id="csv-form">
+        <h3>Crud App</h3>
+
         <input
           type="file"
           accept=".csv"
@@ -124,34 +176,39 @@ const CsvReader = () => {
         <button
           onClick={(e) => {
             e.preventDefault();
-            // if (csvFile) submit();
-            submit();
+            if (csvFile) submit();
           }}
         >
           submit
         </button>
-        {errorMsg}
+
+        {/* <button
+          onClick={(e) => {
+            e.preventDefault();
+            download();
+          }}
+        >
+          export
+        </button> */}
         <br />
         {csvArray.length > 0 ? (
           <>
             <table>
               <thead>
-                <tr>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Age</th>
-                  <th>Sex</th>
-                  <th>Action</th>
-                </tr>
+                {headers.map((item) => (
+                  <th>{item}</th>
+                ))}
               </thead>
               <tbody>
                 {csvArray.map((item, i) => (
                   <tr key={item.id}>
+                    <td>{item.id}</td>
                     <td>{item.firstName}</td>
                     <td>{item.lastName}</td>
                     <td>{item.age}</td>
                     <td>{item.sex}</td>
-                    {item.firstName === undefined ? null : (
+
+                    {item.firstName === "" ? undefined : (
                       <td>
                         <FiTrash2
                           onClick={() => handleDelete(item.id)}
